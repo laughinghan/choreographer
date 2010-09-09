c57b65aec33ba7200d546d6281ec37fe9a45293f
Han Wei <laughinghan@gmail.com>
2010-09-09 00:52:08 -0700

    vastly simplify API in light of .exportTo being impossible

--- README.md
+++ README.md
@@ -9,65 +9,32 @@ Usage
 
 Dirt simple:
 
-    var http = require('http');
-    require('choreographer').exportTo(this);
+    var http = require('http'), serve = require('choreographer').serve();
     
-    get('/chatroom/*/messages', function(request, response, room)
+    serve.get('/chatroom/*/messages', function(request, response, room)
     {
       response.writeHead(200, {'Content-Type': 'text/plain'});
       response.end('No messages in ' + room + '.\n');
     });
     
-    post('/chatroom/*/message', function(request, response, room)
+    serve.post('/chatroom/*/message', function(request, response, room)
     {
       response.writeHead(200, {'Content-Type': 'text/plain'});
       response.end('Posted message to ' + room + '.\n');
     });
     
-    notFound(function(request, response)
+    serve.notFound(function(request, response)
     {
       response.writeHead(404, {'Content-Type': 'text/plain'});
       response.end('404: This server is just a skeleton for a chat server.\n' +
         'I\'m afraid ' + request.url + ' cannot be found here.\n');
     });
     
-    serve(http).listen(80);
+    http.createServer(serve).listen(80);
 
 As in Sinatra, routes are first-come, first-serve (only the callback for the
 first route to be matched by a request is invoked, and routes are matched in the
 order they are defined).
-
-If you want to avoid polluting the global namespace, don't call `.exportTo()`:
-
-    var http = require('http'),
-      router = require('choreographer');
-    
-    router.get('/hw*', function(req, res, hwNum)
-    {
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('Homework ' + hwNum + ' isn\'t up yet.\n');
-    });
-    
-    router.serve(http).listen(80);
-
-`serve` is just a shortcut, so if you want a server that does more than route:
-
-    var http = require('http');
-    require('choreographer').exportTo(this);
-    
-    notFound(function(req, res)
-    {
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.end('Error 404: Not Found\n' +
-        'Actually nothing can be found on this server. It\'s just an example.');
-    });
-    
-    http.createServer(function(req, res)
-    {
-      //serve stuff
-      choreography.apply(this, arguments);
-      //serve more stuff
-    }).listen(80);
 
 Easy to use.
 
--- choreographer.js
+++ choreographer.js
@@ -8,78 +8,72 @@
  *
  */
 
-//export the Choreographer module's API to `api`
-exports.exportTo = function(api)
+//creates router
+exports.serve = function()
 {
-  for(var method in exports)
-    api[method] = exports[method];
-};
-
-//shortcut for `http.createServer(choreography)`
-exports.serve = function(http)
-{
-  return http.createServer(exports.choreography);
-};
-
-//to be passed to `require('http').createServer()`
-exports.choreography = function(req, res)
-{
-  var url = req.url, _routes = routes[req.method];
-  for(var route in _routes)
+  //routing server, to be passed to `require('http').createServer()`
+  var serve = function(req, res)
   {
-    route = _routes[route]; //I don't want the damn index!
-
-    var matches = url.match(route);
-    if(matches) //say '/foo/asdf/jkl' has matched '/foo/*/*'
+    var url = req.url, _routes = routes[req.method];
+    for(var route in _routes)
     {
-      //then turn arguments from [req,res] into [req,res,'asdf','jkl']
-      __Array_push.apply(arguments, matches);
-      return route.callback.apply(this, arguments);
+      route = _routes[route]; //I don't want the damn index!
+
+      var matches = url.match(route);
+      if(matches) //say '/foo/asdf/jkl' has matched '/foo/*/*'
+      {
+        //then turn arguments from [req,res] into [req,res,'asdf','jkl']
+        __Array_push.apply(arguments, matches);
+        return route.callback.apply(this, arguments);
+      }
     }
-  }
-  //route not found: no route has matched and hence returned yet
-  notFoundHandler.apply(this, arguments);
-};
-var __Array_push = [].push;
-
-//dictionary of arrays of routes
-var routes = {};
-
-//routing API
-['HEAD', 'GET', 'POST', 'PUT', 'DELETE']
-.forEach(function(method)
-{
-  routes[method] = [];
-
-  exports[method.toLowerCase()] =
-  exports[method] =
-  function(route, callback) //e.g. router.get('/foo/*',function(req,res,bar){});
-  {
-    if(route instanceof RegExp) //if supplied route is already a RegExp,
-      route = new RegExp(route); //just clone it
-    else //else stringify and interpret as regex where * matches URI segments
-      route = new RegExp('^' + //and everything else matches literally
-        String(route).replace(specialChars, '\\$&').replace('*', '([^/?#]*)')
-      + '(?:[?#].*)?$');
-    route.callback = callback;
-    routes[method].push(route);
+    //route not found: no route has matched and hence returned yet
+    notFoundHandler.apply(this, arguments);
   };
-});
-//special characters that need to be escaped when passed to `RegExp()`, lest
-//they be interpreted as pattern-matching:
-var specialChars = /[|.+?{}()\[\]^$]/g;
 
-//404 is a route too
-exports.notFound = function(handler)
-{
-  notFoundHandler = handler;
+  //dictionary of arrays of routes
+  var routes = {};
+
+  //routing API
+  ['HEAD', 'GET', 'POST', 'PUT', 'DELETE']
+  .forEach(function(method)
+  {
+    routes[method] = [];
+
+    serve[method.toLowerCase()] =
+    function(route, callback) //e.g. router.get('/foo/*',function(req,res,bar){});
+    {
+      if(route instanceof RegExp) //if supplied route is already a RegExp,
+        route = new RegExp(route); //just clone it
+      else //else stringify and interpret as regex where * matches URI segments
+        route = new RegExp('^' + //and everything else matches literally
+          String(route).replace(specialChars, '\\$&').replace('*', '([^/?#]*)')
+        + '(?:[?#].*)?$');
+      route.callback = callback;
+      routes[method].push(route);
+    };
+  });
+  //special characters that need to be escaped when passed to `RegExp()`, lest
+  //they be interpreted as pattern-matching:
+  var specialChars = /[|.+?{}()\[\]^$]/g;
+
+  //404 is a route too
+  serve.notFound = function(handler)
+  {
+    notFoundHandler = handler;
+  };
+
+  //handles requests where no matching route is found
+  var notFoundHandler = defaultNotFound;
+
+  return serve;
 };
+var __Array_push = [].push; //Array.prototype.push, used by `serve()`
 
-//handles requests where no matching route is found
-var notFoundHandler = function(req, res)
+function defaultNotFound(req, res)
 {
   res.writeHead(404, 'Not Found', { 'Content-Type': 'text/html' });
   res.end('<html><head><title>Error 404: Not Found</title></head><body>' +
     '<h1>Error 404: Not Found</h1>' +
     '<p>Cannot ' + req.method + ' ' + req.url + '</body></html>');
-};
+}
