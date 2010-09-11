9b4159dbcaa553d6eee75a129d64eec76249d815
Han Wei <laughinghan@gmail.com>
2010-09-10 21:12:03 -0700

    add newlines to 404

--- choreographer.js
+++ choreographer.js
@@ -80,7 +80,7 @@ var __Array_push = [].push; //Array.prototype.push, used by `server()`
 function defaultNotFound(req, res)
 {
   res.writeHead(404, { 'Content-Type': 'text/html' });
-  res.end('<html><head><title>Error 404: Not Found</title></head><body>' +
-    '<h1>Error 404: Not Found</h1>' +
-    '<p>Cannot ' + req.method + ' ' + req.url + '</body></html>');
+  res.end('<html><head><title>Error 404: Not Found</title></head><body>\n' +
+    '<h1>Error 404: Not Found</h1>\n' +
+    '<p>Cannot ' + req.method + ' ' + req.url + '</body></html>\n');
 }
